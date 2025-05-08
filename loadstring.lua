1dce5d15c163d8c2f21a09bb2e78606c3fce541a
ImaMeow <100092400+ImaMeow@users.noreply.github.com>
2025-05-08 18:22:15 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://github.com/ImaMeow/meowhook/releases/latest/download/meowhook-obf.lua'))()
+loadstring(game:HttpGet('https://github.com/ImaMeow/meowhook/releases/latest/download/meowhook-deobf.lua'))()
